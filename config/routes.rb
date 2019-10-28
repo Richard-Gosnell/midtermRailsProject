b3cfd0e106950bc1fd7a76e36b871554a54c9c65
Richard-Gosnell <richardshanegosnell@gmail.com>
2019-10-27 21:29:41 -0500

    Home page works,

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pokemon#index'
+
+  resource :pokemon do
+    member do
+      # get 'index'
+      get 'about'
+    end
+  end
 end
