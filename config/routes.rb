a5b5b2ded95c7600553ca39b509388d449fa184d
Richard-Gosnell <richardshanegosnell@gmail.com>
2019-10-28 20:30:37 -0500

    About page broken in nav menu

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pokemon#index'
-  get '/pokemon/about', to: 'pokemon#about'
   get '/pokemon/index', to: 'pokemon#index'
+  get '/pokemon/about', to: 'pokemon#about'
 
   resource :pokemon do
     member do
